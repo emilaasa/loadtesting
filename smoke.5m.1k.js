987870bc0a6d17d9e1de8e22de6a1bdc3f2bc8bd
Carl Loa Odin <loa@nonetype.io>
2020-04-05 16:03:19 +0200

    Add more tests

--- smoke.5m.1k.js
+++ smoke.5m.1k.js
@@ -6,10 +6,8 @@ export const options = {
   noVUConnectionReuse: true,
   insecureSkipTLSVerify: true,
   stages: [
-    { duration: "1m", target: 500 }, // get a baseline
-    { duration: "1m", target: 1500 }, // ramp up
-    { duration: "3m", target: 3000 },
-    { duration: "3m", target: 5000 }, // peak rps
+    { duration: "1m", target: 1000 }, // get a baseline
+    { duration: "3m", target: 1000 }, // peak vu
     { duration: "1m", target: 0 } // recovery
   ],
   ext: {
@@ -29,15 +27,19 @@ export default function() {
   let res = http.get(`${baseURL}/`);
 
   // Fetch static assets in parallell like a browser would
-  http.batch([
-    ["GET", `${baseURL}/css/site.dadf2a10de6a036d4055.css`],
-    ["GET", `${baseURL}/folkhalsomyndigheten.png`],
-    ["GET", `${baseURL}/msb.png`],
-    ["GET", `${baseURL}/favicon.ico`],
-    ["GET", `${baseURL}/js/1.site.dadf2a10de6a036d4055.bundle.js`],
-    ["GET", `${baseURL}/js/site.dadf2a10de6a036d4055.bundle.js`],
-    ["GET", `${baseURL}/socialstyrelsen.png`]
-  ]);
+  try {
+    http.batch([
+      ["GET", `${baseURL}/css/site.4713dd1d06b9379dc472.css`],
+      ["GET", `${baseURL}/js/1.site.4713dd1d06b9379dc472.bundle.js`],
+      ["GET", `${baseURL}/js/site.4713dd1d06b9379dc472.bundle.js`],
+      ["GET", `${baseURL}/favicon.ico`],
+      ["GET", `${baseURL}/folkhalsomyndigheten.png`],
+      ["GET", `${baseURL}/msb.png`],
+      ["GET", `${baseURL}/socialstyrelsen.png`]
+    ]);
+  } catch (err) {
+  	console.warn(err);
+  }
 
   sleep(1);
 }
